7d0b118c4388d47262d9335a1e2aa2663ae07033
DaniAffCH <danieleaffinita2000@gmail.com>
2023-10-14 18:20:35 +0200

    refactoring

--- assignment_1/behavior_realizer.py
+++ assignment_1/behavior_realizer.py
@@ -8,6 +8,7 @@ import tempfile
 
 from math import pi
 
+
 class WavingSkill():
 
     """
@@ -33,9 +34,9 @@ class WavingSkill():
         Performs the actual FSM commanding joint angles for execTime seconds
     """
 
-    def __init__(self, pepperInstance, speed=.3, waveAngle = 45) -> None:
-        assert(speed >= 0.0 and speed <= 1.0)
-        assert(waveAngle > 0 and waveAngle < 90)
+    def __init__(self, pepperInstance, speed=.3, waveAngle=45) -> None:
+        assert (speed >= 0.0 and speed <= 1.0)
+        assert (waveAngle > 0 and waveAngle < 90)
 
         self.speed = speed
         # The variable self.state represents the current FSM state
@@ -43,13 +44,11 @@ class WavingSkill():
         self.pepper = pepperInstance
         self.waveAngle = waveAngle
 
-
-    def degtorad(self,x):
+    def degtorad(self, x):
         return x*pi/180
-    
 
     # Basic FSM Diagram
-    # 1 -> 2 -> 3 -> 4 
+    # 1 -> 2 -> 3 -> 4
     #           ^    |
     #           |____|
 
@@ -59,52 +58,58 @@ class WavingSkill():
 
             # Set the right shoulder in default position
             if self.state == 0:
-                # Setting the desired joint angle 
+                # Setting the desired joint angle
                 target = self.degtorad(0)
                 # Command the execution
                 self.pepper.setAngles("RShoulderPitch", target, 1.)
-                # Error between the desired joint angle and the actual one 
-                error = abs(self.pepper.getAnglesPosition("RShoulderPitch") - target)
+                # Error between the desired joint angle and the actual one
+                error = abs(self.pepper.getAnglesPosition(
+                    "RShoulderPitch") - target)
                 # If the error is small enough, then execute a state transition
-                if error < 1e-2 : 
+                if error < 1e-2:
                     self.state = 1
 
             # Moves the arm to right direction rotating the shoulder
             elif self.state == 1:
                 target = self.degtorad(-90)
                 self.pepper.setAngles("RShoulderRoll", target, 1.)
-                error = abs(self.pepper.getAnglesPosition("RShoulderRoll") - target)
-                if error < 1e-2 : 
+                error = abs(self.pepper.getAnglesPosition(
+                    "RShoulderRoll") - target)
+                if error < 1e-2:
                     self.state = 2
 
             # Flip the arm facing upward
             elif self.state == 2:
                 target = self.degtorad(-90)
                 self.pepper.setAngles("RShoulderPitch", target, 1.)
-                error = abs(self.pepper.getAnglesPosition("RShoulderPitch") - target)
-                if error < 1e-2 : 
+                error = abs(self.pepper.getAnglesPosition(
+                    "RShoulderPitch") - target)
+                if error < 1e-2:
                     self.state = 3
 
             # Bend the elbow to 90 degree (first waving step)
             elif self.state == 3:
                 target = self.degtorad(90)
                 self.pepper.setAngles("RElbowRoll", target, self.speed)
-                error = abs(self.pepper.getAnglesPosition("RElbowRoll") - target)
-                if error < 1e-2 : 
+                error = abs(self.pepper.getAnglesPosition(
+                    "RElbowRoll") - target)
+                if error < 1e-2:
                     self.state = 4
-            
+
             # Bend the elbow back to 90-self.waveAngle degree (second waving step).
             # Once this last state is executed, the following state will be again the state 3
             elif self.state == 4:
                 target = self.degtorad(90-self.waveAngle)
                 self.pepper.setAngles("RElbowRoll", target, self.speed)
-                error = abs(self.pepper.getAnglesPosition("RElbowRoll") - target)
-                if error < 1e-2 : 
+                error = abs(self.pepper.getAnglesPosition(
+                    "RElbowRoll") - target)
+                if error < 1e-2:
                     self.state = 3
 
         # Finally go back to the base configuration again
         self.pepper.goToPosture("StandZero", 1.)
         self.state = 0
+
 
 class SaySkill():
     """
@@ -120,7 +125,7 @@ class SaySkill():
     def __call__(self, text):
         # Creating a temporary file which will be deleted as soon as the associated object is closed
         with tempfile.NamedTemporaryFile(suffix=".mp3") as tmpFile:
-            # Write the tts file 
+            # Write the tts file
             gtts.gTTS(text).save(tmpFile.name)
             # Execute the tts file
             playsound(tmpFile.name)
@@ -132,46 +137,48 @@ class BehaviorRealizer():
         # Loading Robot and  Ground
         simulation_manager = SimulationManager()
         client = simulation_manager.launchSimulation(gui=True)
-        self.pepper = simulation_manager.spawnPepper(client, spawn_ground_plane=True)
+        self.pepper = simulation_manager.spawnPepper(
+            client, spawn_ground_plane=True)
         self.theSaySkill = SaySkill()
         self.theWavingSkill = WavingSkill(self.pepper)
 
     def say(self, text):
-        threading.Thread(target=self.theSaySkill,args=(text,)).start()
+        threading.Thread(target=self.theSaySkill, args=(text,)).start()
 
     def waving(self, execTime=5):
-        threading.Thread(target=self.theWavingSkill,args=(execTime,)).start()
-    
+        threading.Thread(target=self.theWavingSkill, args=(execTime,)).start()
+
 
 if __name__ == "__main__":
 
-    behavior_realizer_class = BehaviorRealizer()  
-    
-    INPUT_OPTIONS=["done", "waving", "say"]
-        
+    behavior_realizer_class = BehaviorRealizer()
+
+    INPUT_OPTIONS = ["done", "waving", "say"]
+
     repeat_ = True
     while repeat_:
         user_input = input("INPUT : ")
-        user_input = user_input.split(" ",1)
-                
-        if user_input[0]==INPUT_OPTIONS[0]:
-            repeat_=False
-        
-        elif user_input[0]==INPUT_OPTIONS[1]:
+        user_input = user_input.split(" ", 1)
+
+        if user_input[0] == INPUT_OPTIONS[0]:
+            repeat_ = False
+
+        elif user_input[0] == INPUT_OPTIONS[1]:
             print("Executing waving")
-            if len(user_input)>1 and user_input[1].isnumeric():
+            if len(user_input) > 1 and user_input[1].isnumeric():
                 behavior_realizer_class.waving((int)(user_input[1]))
             else:
-                print("\n[TIP: you can also specify the duration of the waving by typing 'waving <TimeInSeconds>', e.g. 'waving 5']\n")
+                print(
+                    "\n[TIP: you can also specify the duration of the waving by typing 'waving <TimeInSeconds>', e.g. 'waving 5']\n")
                 behavior_realizer_class.waving()
-        
-        elif user_input[0]==INPUT_OPTIONS[2]:
+
+        elif user_input[0] == INPUT_OPTIONS[2]:
             print("Executing say")
             if len(user_input) > 1 and user_input[1]:
                 behavior_realizer_class.say(user_input[1])
             else:
-                print("\nPlease specify the sentence to say. You can write 'say <sentence>', e.g. 'say hello world'\n")
-        
+                print(
+                    "\nPlease specify the sentence to say. You can write 'say <sentence>', e.g. 'say hello world'\n")
+
         if not user_input[0] in INPUT_OPTIONS:
             print("Please enter 'done' to exit.")
-        
