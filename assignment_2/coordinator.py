35716183c16b9e765646c353c9b5320544865ba8
DaniAffCH <danieleaffinita2000@gmail.com>
2023-10-19 12:52:24 +0200

    rough initial setup

---
+++ assignment_2/coordinator.py
@@ -0,0 +1,11 @@
+from BLMparser import Parser
+
+class Coordinator():
+    def __init__(self, parser:Parser) -> None:
+        pass
+
+    def spawn(self):
+        pass
+
+    def join(self):
+        pass
---
+++ assignment_2/main.py
@@ -0,0 +1,4 @@
+from BLMparser import Parser
+
+if __name__ == "__main__":
+    parser = Parser("test.json")
